ab33831dd62a8c945b366e12e607180232b2a129
Kishan Mehta <kishan@scrapinghub.com>
2021-09-17 17:34:16 +0530

    Add a small method.

--- scrapypoet_poc/pages.py
+++ scrapypoet_poc/pages.py
@@ -1,6 +1,7 @@
 from web_poet.pages import ItemWebPage, WebPage
 
 # ------ Base page objects ------
+from scrapypoet_poc.helpers import _first_or_none
 
 
 class JobListPage(WebPage):
@@ -22,10 +23,19 @@ class GlassdoorJobListPage(JobListPage):
 
 
 class GlassdoorJobsPage(JobPage):
+    def get_job_id(self):
+        qdata = self.meta.get('qdata') or self.meta.get('old_item') or {}
+
+        return _first_or_none(
+            self.css('.empLinks ::attr(data-jobid)').getall() or
+            self.css('.empLinks ::attr(data-job-id)').getall() or
+            self.css('.applyButton ::attr(data-job-id)').getall() or
+            [qdata.get('job_id')])
+
     def to_item(self):
         return {
             "last_visited_url": self.url.strip("/"),
             "url": self.url,
+            "job_id": self.get_job_id(),
             "name": self.css("title::text").get(),
         }
-
